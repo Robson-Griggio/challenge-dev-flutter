7f642d48aed28766e5994a9132a4d9882bb5e0e2
Robson <robsongriggio@gmail.com>
2025-07-20 15:29:46 -0300

    feat: implement routing with GoRouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:challenge_dev_flutter/ui/home_page/view_model/home_page_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MainApp());
 }
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomePageScreen()),
+  ],
+);
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
-    );
+    return MaterialApp.router(routerConfig: _router);
   }
 }
--- lib/utils/result_pattern.dart
+++ lib/utils/result_pattern.dart
@@ -2,13 +2,11 @@ sealed class Result<T> {
   const Result();
 }
 
-/// Represents a successful result containing a value of type [T].
 final class Ok<T> extends Result<T> {
   final T value;
   const Ok(this.value);
 }
 
-/// Represents an error result containing an error message or exception.
 final class Error<T> extends Result<T> {
   final Object error;
   final StackTrace? stackTrace;
